6e3091f88724d050e65da65dd8ff4c41a672043c
Owain <owaingdwilliams@gmail.com>
2020-12-30 12:34:47 +0000

    Refactor Guys App

--- GuysApp/GuysApp/Program.cs
+++ GuysApp/GuysApp/Program.cs
@@ -2,7 +2,7 @@
 
 namespace GuysApp
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
@@ -17,16 +17,16 @@ namespace GuysApp
                 Console.Write("Enter an amount: ");
                 string howMuch = Console.ReadLine();
                 if (howMuch == "") return;
-                if(int.TryParse(howMuch, out int amount))
+                if (int.TryParse(howMuch, out int amount))
                 {
                     Console.Write("Who should give the cash: ");
                     string whichGuy = Console.ReadLine();
-                    if(whichGuy == "Joe")
+                    if (whichGuy == "Joe")
                     {
-                     int cashGiven = joe.GiveCash(amount);
+                        int cashGiven = joe.GiveCash(amount);
                         bob.ReceiveCash(cashGiven);
                     }
-                    else if(whichGuy == "Bob")
+                    else if (whichGuy == "Bob")
                     {
                         int cashGiven = bob.GiveCash(amount);
                         joe.ReceiveCash(cashGiven);
@@ -41,48 +41,6 @@ namespace GuysApp
                     Console.WriteLine("Please enter an amount (or a blank line to exit).");
                 }
             }
-        }
-
-        class Guy
-        {
-            public string Name;
-            public int Cash;
-
-            public void WriteMyInfo()
-            {
-                Console.WriteLine(Name + " has " + Cash + " bucks.");
-            }
-
-            public int GiveCash(int amount)
-            {
-                if(amount <= 0)
-                {
-                    Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
-                    return 0;
-                }
-                if(amount > Cash)
-                {
-                    Console.WriteLine(Name + " says: " +
-                        "I don't have enough cash to give you " + amount);
-                    return 0;
-                }
-                Cash -= amount;
-                return amount;
-            }
-
-            public void ReceiveCash(int amount)
-            {
-                if(amount <= 0)
-                {
-                    Console.WriteLine(Name + " says: " + amount + " isn't an amount I'll take");
-                    
-                }
-                else
-                {
-                    Cash += amount;
-                }
-            }
-                
         }
     }
 }
